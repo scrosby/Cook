b55fd43823b764e9cbd21d7c77ddba1d4b8c5525
Scott Crosby <scrosby@twosigma.com>
2018-03-14 11:13:48 -0500

    Isolate and metric this lazy evaluation.
    For handle-resource-offers, we can blame datomic as this evaluation can
    occur in the context of the transact-task-duration timer.

--- scheduler/src/cook/mesos/scheduler.clj
+++ scheduler/src/cook/mesos/scheduler.clj
@@ -708,6 +708,7 @@
 
 (timers/deftimer [cook-mesos scheduler handle-resource-offer!-duration])
 (timers/deftimer [cook-mesos scheduler handle-resource-offer!-transact-task-duration])
+(timers/deftimer [cook-mesos scheduler handle-resource-offer!-process-task-txns-duration])
 (timers/deftimer [cook-mesos scheduler handle-resource-offer!-process-matches-duration])
 (timers/deftimer [cook-mesos scheduler handle-resource-offer!-mesos-submit-duration])
 (timers/deftimer [cook-mesos scheduler handle-resource-offer!-match-duration])
@@ -853,7 +854,9 @@
   "Updates the state of matched tasks in the database and then launches them."
   [matches conn db driver fenzo framework-id mesos-run-as-user]
   (let [matches (map #(update-match-with-task-metadata-seq % db framework-id mesos-run-as-user) matches)
-        task-txns (matches->task-txns matches)]
+        task-txns (timers/time!
+                      handle-resource-offer!-process-task-txns-duration
+                      (doall (matches->task-txns matches)))]
     ;; Note that this transaction can fail if a job was scheduled
     ;; during a race. If that happens, then other jobs that should
     ;; be scheduled will not be eligible for rescheduling until
